c846232c1f773da52bcf387bca53a7535b5e7249
Sheromi Paramanathan <117504958+SheromiParamanathan@users.noreply.github.com>
2026-07-14 13:56:28 +0530

    Debugged Theme Switcher

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="light" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body className="relative">
         <ThemeProvider>
           <CustomCursor />
